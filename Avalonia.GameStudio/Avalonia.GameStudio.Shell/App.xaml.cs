3e83b62a7603cce98f90d18ffb89f1371407b8a2
Nicolas Musset <musset.nicolas@gmail.com>
2019-06-23 18:12:12 +0900

    Add solution and default project
    Created from Avalonia MVVM template.
    Change target frameworks to .NET Core 3.1 and .NET Framework 4.8

---
+++ Avalonia.GameStudio/Avalonia.GameStudio.Shell/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.GameStudio.Shell.ViewModels;
+using Avalonia.GameStudio.Shell.Views;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.GameStudio.Shell
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ Avalonia.GameStudio/Avalonia.GameStudio.Shell/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Avalonia.GameStudio.Shell.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
