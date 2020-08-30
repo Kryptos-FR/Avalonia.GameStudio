b646057148be53fdb184c1bb6dd53c6e2442a571
Nicolas Musset <musset.nicolas@gmail.com>
2020-08-30 17:25:06 +0900

    [Core] Add Core library

---
+++ Avalonia.GameStudio/Avalonia.GameStudio.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Avalonia.GameStudio.Core.Extensions
+{
+    public static class TaskExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Forget(this Task task)
+        {
+            if (task == null) throw new ArgumentNullException();
+        }
+    }
+}
