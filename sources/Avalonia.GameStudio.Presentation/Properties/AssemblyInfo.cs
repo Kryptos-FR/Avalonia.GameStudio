ed868ef1af2a499fdc5d2c876c37f67239ed4452
Nicolas Musset <musset.nicolas@gmail.com>
2020-09-05 18:56:05 +0900

    [Presentation] Add test project

--- sources/Avalonia.GameStudio.Presentation/Properties/AssemblyInfo.cs
+++ sources/Avalonia.GameStudio.Presentation/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Metadata;
+﻿using System.Runtime.CompilerServices;
+
+using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/kryptos-fr/avalonia.gamestudio", "Avalonia.GameStudio.Presentation")]
 [assembly: XmlnsDefinition("https://github.com/kryptos-fr/avalonia.gamestudio", "Avalonia.GameStudio.Presentation.MarkupExtensions")]
+
+[assembly: InternalsVisibleTo("Avalonia.GameStudio.Presentation.Tests")]
---
+++ tests/Avalonia.GameStudio.TestHelpers/README.md
@@ -0,0 +1,3 @@
+﻿We follow the same guidelines as AspNetCore.
+
+https://github.com/dotnet/aspnetcore/wiki/Engineering-guidelines#unit-tests-and-functional-tests
