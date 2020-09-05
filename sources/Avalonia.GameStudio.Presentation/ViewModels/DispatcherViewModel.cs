21fb047a7ef6021f80ff5770c12818eded0bb014
Nicolas Musset <musset.nicolas@gmail.com>
2020-09-05 14:01:41 +0900

    [skip]

---
+++ sources/Avalonia.GameStudio.Presentation/ViewModels/DispatcherViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Threading;
+
+namespace Avalonia.GameStudio.Presentation.ViewModels
+{
+    public abstract class DispatcherViewModel : ViewModelBase
+    {
+        /// <inheritdoc />
+        protected override void OnPropertyChanging(string propertyName)
+        {
+#if !DEBUG
+            if (!HasPropertyChangingSubscriber) return;
+#endif
+            if (Dispatcher.UIThread.CheckAccess())
+                base.OnPropertyChanging(propertyName);
+
+            Dispatcher.UIThread.Post(() => base.OnPropertyChanging(propertyName));
+        }
+
+        /// <inheritdoc />
+        protected override void OnPropertyChanged(string propertyName)
+        {
+#if !DEBUG
+            if (!HasPropertyChangedSubscriber) return;
+#endif
+            if (Dispatcher.UIThread.CheckAccess())
+                base.OnPropertyChanged(propertyName);
+
+            Dispatcher.UIThread.Post(() => base.OnPropertyChanged(propertyName));
+        }
+    }
+}
---
+++ sources/Avalonia.GameStudio.Shell/ViewModels/DirectoryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+using Avalonia.GameStudio.Presentation.ViewModels;
+
+namespace Avalonia.GameStudio.Shell.ViewModels
+{
+    internal sealed class DirectoryViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<DirectoryViewModel> _subDirectories = new ObservableCollection<DirectoryViewModel>();
+
+        public DirectoryViewModel()
+        {
+            SubDirectories = new ReadOnlyObservableCollection<DirectoryViewModel>(_subDirectories);
+        }
+
+        public DirectoryViewModel? Parent { get; set; }
+
+        public ReadOnlyObservableCollection<DirectoryViewModel> SubDirectories { get; }
+
+        private void UpdateParent(DirectoryViewModel? oldParent, DirectoryViewModel? newParent)
+        {
+            oldParent?._subDirectories.Remove(this);
+            newParent?._subDirectories.Add(this);
+        }
+    }
+}
